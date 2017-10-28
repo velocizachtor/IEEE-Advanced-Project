7e3b1889b8aa64d3795c61893ffb859ec262e852
Wilson Chang <ckwojai@gmail.com>
2017-10-28 16:19:46 -0700

    Add Arduino Code

---
+++ Lab 1/Arduino Code/test.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  RF24::RF24(9,10);
+  RF24::radio.begin();
+  RF24::
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
