9d716d17de106f2b3414db479f9c97ab8204af29
Wilson Chang <ckwojai@gmail.com>
2017-10-28 16:51:21 -0700

    Add Arduino code in test folder

--- Lab 1/Arduino Code/test/test.ino
+++ Lab 1/Arduino Code/test/test.ino
@@ -2,7 +2,9 @@ void setup() {
   // put your setup code here, to run once:
   RF24::RF24(9,10);
   RF24::radio.begin();
-  RF24::
+  RF24::setChannel(16);
+  RF24::setPALevel(RF24_PA_MIN);
+  
 }
 
 void loop() {
