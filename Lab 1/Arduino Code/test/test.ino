b4a468e6f30bdbb281204745478bb1ead2e6d473
Wilson Chang <ckwojai@gmail.com>
2017-10-28 17:25:58 -0700

    Almost done Arduino Code (Part C)

--- Lab 1/Arduino Code/test/test.ino
+++ Lab 1/Arduino Code/test/test.ino
@@ -4,14 +4,16 @@ void setup() {
   radio.begin();
   radio.setChannel(16);
   radio.setPALevel(RF24_PA_MIN);
-  radio.openReadingPipe(,);
-  radio.openWritingPipe();
+  radio.openReadingPipe(1, 0xc2c2c2c2);
+  radio.openWritingPipe(0xe7e7e7e7e7);
   radio.setCRCLength(RF24_CRC_16);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   radio.startListening();
-  
-  
+  if(radio.available()) {
+    radio.read(&data,sizeof(data));
+    radio.stopListening();
+  }   
 }
