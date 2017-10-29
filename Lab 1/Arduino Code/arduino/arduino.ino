4d5d1efa4140c3021dd883ed2efee7c7f3121cf9
Wilson Chang <ckwojai@gmail.com>
2017-10-28 17:47:40 -0700

    Add arduino and teensy

---
+++ Lab 1/Arduino Code/arduino/arduino.ino
@@ -0,0 +1,25 @@
+#include <printf.h>
+#include <nRF24L01.h>
+#include <RF24_config.h>
+#include <RF24.h>
+
+RF24 radio(9,10);
+void setup() {
+  // put your setup code here, to run once: 
+  radio.begin();
+  radio.setChannel(16);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.openReadingPipe(1, 0xc2c2c2c2);
+  radio.openWritingPipe(0xe7e7e7e7);
+  radio.setCRCLength(RF24_CRC_16);  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  radio.startListening();
+  int data[31];
+  if (radio.available()) {
+    radio.read(&data,sizeof(data));
+    radio.stopListening();
+  }   
+}
---
+++ Lab 1/Arduino Code/teensy/teensy.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+  RF24::RF24(9,10) radio;
+  radio.begin();
+  radio.setChannel(16);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.openReadingPipe(1, 0xe7e7e7e7);
+  radio.openWritingPipe(0xc2c2c2c2c);
+  radio.setCRCLength(RF24_CRC_16);  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  radio.startListening();
+  int data[31];
+  if (radio.available()) {
+    radio.read(&data,sizeof(data));
+    radio.stopListening();
+  }   
+}
