5891799b1588d76295b1a2442ac0e71b978735cc
Wilson Chang <ckwojai@gmail.com>
2017-10-30 20:09:17 -0700

    Add test2, test using example

--- Lab 1/Arduino Code/test/arduino/arduino.ino
+++ Lab 1/Arduino Code/test/arduino/arduino.ino
@@ -10,18 +10,25 @@ void setup() {
   radio.begin();
   radio.setChannel(16);
   radio.setPALevel(RF24_PA_MIN);
-  radio.openReadingPipe(1, 0xC2C2C2C2);
-  radio.openWritingPipe(0xE7E7E7E7);
+  radio.openReadingPipe(1, 0xC2C2C2C2C2);
+  radio.openWritingPipe(0xE7E7E7E7E7);
   radio.setCRCLength(RF24_CRC_16);  
+  printf_begin();
+  radio.printDetails();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   radio.startListening();
-  char data;
+  unsigned long data;
   if (radio.available()) {
+    while (radio.available()) {                                   // While there is data ready
+        radio.read( &data, sizeof(unsigned long) );             // Get the payload
+      }
+     
     radio.read(&data, 1);
     radio.stopListening();
-    Serial.print(data);
+    Serial.print("data");
   }   
 }
+
