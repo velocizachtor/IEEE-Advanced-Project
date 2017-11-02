d4262eb6e629dd2c01fb42957edae59f5e8aeee2
velocizachtor <33331598+velocizachtor@users.noreply.github.com>
2017-11-02 16:10:50 -0700

    Spacelab
    Code for arduino

---
+++ Lab 1/spacelab.ino
@@ -0,0 +1,31 @@
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 lights(CE_R, CS_R); // need to map CE_R, CS_R to teensy pins
+RF24 buttons(CE_T, CS_T); // need to map CE_T, CS_T to arduino pins
+uint8_t addresses[]
+
+void setup() {
+  // put your setup code here, to run once:
+  lights.begin();
+  buttons.begin();
+  lights.setChannel(16);
+  buttons.setChannel(16);
+
+  lights.setPALevel(RF24_PA_MIN);
+  buttons.setPALevel(RF24_PA_MIN);
+
+  // open reading and writing pipes.
+  lights.openWritingPipe(0x1a1a1a1a1a);
+  buttons.openReadingPipe(0x1a1a1a1a1a);
+
+  buttons.openWritingPipe(0x5c5c5c5c5c);
+  lights.openReadingPipe(0x5c5c5c5c5c);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
